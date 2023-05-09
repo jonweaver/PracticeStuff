eeed64b3008bd576b13da1c1a1bbcd366a5d38b5
Jonathan Weaver <jonathanweaver@rocketmortgage.com>
2023-05-08 21:23:30 -0400

    add a practice example

---
+++ Practice/Loan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice;
+
+public class Loan
+{
+    public Loan()
+    {
+        LoanId = Faker.RandomNumber.Next().ToString();
+        BorrowerName = Faker.Name.FullName().ToString();
+        Address = new PropertyAddress
+        {
+            City = Faker.Address.City(),
+            State = Faker.Address.UsState(),
+            StreetAddress = Faker.Address.StreetAddress(),
+            Zip = Faker.Address.ZipCode()
+        };
+    }
+
+    public PropertyAddress Address { get; set; }
+
+    public int? Years { get; set; }
+
+    public string BorrowerName { get; set; } = string.Empty;
+
+    public string LoanId { get; set; } = string.Empty;
+
+    public class PropertyAddress
+    {
+        public string City { get; set; } = string.Empty;
+
+        public string State { get; set; } = string.Empty;
+
+        public string StreetAddress { get; set; } = string.Empty;
+
+        public string Zip { get; set; } = string.Empty;
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           //Create a new instance of the Loan class and then serialize it into JSON.
+           //The JSON should be indented.
+           //If any of the properties are null they should not show up in the output
+           //The names of the properties in the JSON output should be camel case.
+
+            // You should write the JSON to a file somewhere on your computer.
+
+            //After you get the JSON file read it back in from the file and Deserialize it back to a Loan.
+
+
+        }
+    }
+}
